b0d82e191c96c57d672b157daac863d422aba80d
MJMayaya2 <mzwamadodamayaya@gmail.com>
2026-03-10 02:57:19 +0200

    Update

--- Geography/Term-1/Grade-11/Games/data/slide116.js
+++
@@ -1,5 +0,0 @@
-(function(){var loadHandler=window['sl_{D7C62B58-896C-4CB4-8CA1-C713285B644B}'];loadHandler&&loadHandler(115, '<div id="spr0_27ec5b2"><div id="spr1_27ec5b2" class="kern slide"><img id="img2_27ec5b2" src="data/img60.png" width="960px" height="540px" alt="" style="left:0px;top:0px;"/><div id="spr3_27ec5b2" aria-label="Ein Bild, das Frau, Schild enthält.\
-\
-Automatisch generierte Beschreibung" role="group"><div id="spr4_27ec5b2"><img id="img0_27ec5b2" src="data/img420.png" width="960" height="540" alt=""/></div></div><div id="spr5_27ec5b2" aria-label="Ein Bild, das Zeichnung enthält.\
-\
-Automatisch generierte Beschreibung" role="group" style="left:215.398px;top:-17.404px;"><div id="spr6_27ec5b2"><img id="img1_27ec5b2" src="data/img421.png" width="592" height="591" alt="" style="left:0.138px;top:-0.014px;"/></div></div></div><div id="spr2_27ec5b2" class="kern slide"><div id="svg0_27ec5b2" style="left:0px;top:0px;"><svg width="960" height="540" viewBox="0 0 960 540"><path fill="#000000" d="M0,0 h960 v540 h-960 Z"/></svg></div></div></div>', '{"s":[]}');})();
--- Geography/Term-1/Grade-11/Games/data/slide124.js
+++
@@ -1,7 +0,0 @@
-(function(){var loadHandler=window['sl_{D7C62B58-896C-4CB4-8CA1-C713285B644B}'];loadHandler&&loadHandler(123, '<div id="spr0_27f2883"><div id="spr1_27f2883" class="kern slide"><img id="img3_27f2883" src="data/img60.png" width="960px" height="540px" alt="" style="left:0px;top:0px;"/><div id="spr3_27f2883" aria-label="Ein Bild, das Frau, Schild enthält.\
-\
-Automatisch generierte Beschreibung" role="group"><div id="spr4_27f2883"><img id="img0_27f2883" src="data/img420.png" width="960" height="540" alt=""/></div></div><div id="spr5_27f2883" aria-label="Ein Bild, das Zeichnung enthält.\
-\
-Automatisch generierte Beschreibung" role="group" style="left:215.398px;top:-17.404px;"><div id="spr6_27f2883"><img id="img1_27f2883" src="data/img421.png" width="592" height="591" alt="" style="left:0.138px;top:-0.014px;"/></div></div></div><div id="spr2_27f2883" class="kern slide"><div id="spr7_27f2883" aria-label="Ein Bild, das Zug enthält.\
-\
-Automatisch generierte Beschreibung" role="group" style="left:0.002px;"><div id="spr8_27f2883"><img id="img2_27f2883" src="data/img452.jpg" width="959.998" height="539.899" alt=""/></div></div></div></div>', '{"s":[]}');})();
